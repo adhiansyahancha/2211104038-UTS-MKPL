1626d06cd59a4390175cc4c7b34fe9ab2e8d4f57
Adhiansyah Ancha <69331906+adhiansyahancha@users.noreply.github.com>
2025-04-17 20:40:06 +0700

    Menyingkat nama parameter; Mengekstrak beberapa ekspresi

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -14,31 +14,22 @@ public class TaxFunction {
 	 * 
 	 */
 	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
+	public static int calculateTax(int monthlySalary, int otherSalary, int monthsWorked, int deductible, boolean isMarried, int numberOfKids) {
+		if (monthsWorked > 12) {
 			System.err.println("More than 12 month working per year");
 		}
 		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
+		int tax = 0;
+		int baseNetIncome = ((monthlySalary + otherSalary) * monthsWorked) - deductible;
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			int deducedNumberOfKids = numberOfKids > 3 ? 3 : numberOfKids
+			tax = (int) Math.round(0.05 * ((baseNetIncome - (54000000 + 4500000 + (deducedNumberOfKids * 1500000))));
 		}
 		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+		tax = (int) Math.round(0.05 * ((baseNetIncome - 54000000));
+		
+		return tax < 0 ? 0 : tax;
 	}
 	
 }
